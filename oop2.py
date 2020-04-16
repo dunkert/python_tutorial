f4d4afb9ae2d58d3e038f80082a74e0d1e8ddd46
Daniel Jean-Baptiste <daniel.jeanbaptiste@gmail.com>
2020-04-15 22:27:03 -0400

    example correction

--- oop2.py
+++ oop2.py
@@ -9,22 +9,23 @@ __status__ = "Production"
 class Animal:
 	def speak(self): return self.sound
 	def legs(self): return  self.number_of_legs
-	def name(self): return self.name
+	def name(self): return self.__class__.__name__
+	def whoami(self): print("A {} has {} and {}".format(self.name(), self.number_of_legs, self.sound))
 
 class duck(Animal):
 	sound = "quacks"
-	legs =  "2 legs"
-	name = "duck"
+	number_of_legs =  "2 legs"
+	#name = "duck"
 
 class human(Animal):
 	sound = "speaks"
-	legs = "2 legs"
-	name =  "human"
+	number_of_legs = "2 legs"
+	#name =  "human"
 
 class dog(Animal):
 	sound = "barks"
-	legs = "4 legs"
-	name =  "dog"
+	number_of_legs = "4 legs"
+	#name =  "dog"
 
 
 def main():
@@ -34,6 +35,11 @@ def main():
 	dk = duck()
 
 	for n in (dg, hm , dk):
-		print("A {} has {} and {}".format(n.name, n.legs, n.sound))
+		print("A {} has {} and {}".format(n.name(), n.legs(), n.speak()))
+
+	print("Trying second method:")
+
+	for n2 in (dg, hm , dk):
+		n2.whoami()
 
 if __name__ == "__main__": main()
