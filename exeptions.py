9b3fcaf02db713cc65fbf3899e59d17285ded1c4
Daniel Jean-Baptiste <daniel.jeanbaptiste@gmail.com>
2020-04-15 22:35:42 -0400

    '

---
+++ exeptions.py
@@ -0,0 +1,15 @@
+__author__ = "Daniel Jean-Baptiste"
+__copyright__ = "Copyright 2017, The Smarter Engineering Project"
+
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Daniel Jean-Baptiste"
+__status__ = "Production"
+
+
+filename = "error.logs" 
+
+try:
+    fh = open(filename,"r")
+except IOError as e:
+    print(e);
