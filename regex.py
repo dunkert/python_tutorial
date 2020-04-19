27039d456ef858b94c1ea1122b6ce1f6928741c1
Daniel Jean-Baptiste <daniel.jeanbaptiste@gmail.com>
2020-04-18 22:12:29 -0400

    Added examples

--- regex.py
+++ regex.py
@@ -5,3 +5,29 @@ __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = "Daniel Jean-Baptiste"
 __status__ = "Production"
+
+
+import re
+
+myList = ["The year is 1999","The year is 2000","The year is 1998","The year is 1997","The year is 2001","The year is 2002"]
+
+for i in sorted(myList):
+    print(i)
+
+for i in sorted(myList):
+    if re.search("(20??)",i):
+        print(i)
+else:
+    print("Nothing else to find")
+
+print("\nSearch for the occurence of 'e' in ", myList[0])
+
+print(re.findall("e",myList[0]))
+
+print(re.sub("bad","good","This is very bad"))
+
+print(re.split("\s","This is a very long text"))
+
+
+
+
